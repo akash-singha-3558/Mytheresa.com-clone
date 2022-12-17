54dc81b3933e0f476a3ae94c03a13fba85f23774
akash-singha-3558 <akashsingha@gmail.com>
2022-12-17 23:55:24 +0530

    update

---
+++ fashion-bag/src/Components/Mans/Man.jsx
@@ -0,0 +1,21 @@
+import ItemBar from "../GlobalComponents/Itembar";
+import Logo from "../Logo";
+import SaleText from "../SaleText";
+import HeroImage1 from "./HeroImage1";
+const Man=()=>{
+
+    return(
+        <>
+        
+        <Logo/>
+    <ItemBar/>
+    <SaleText/>
+        <HeroImage1/>
+        </>
+  
+    
+    
+    )
+    
+    }
+    export default Man;
